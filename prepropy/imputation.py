95300c49130953401d892cace3555836987421d3
Jason Chang <ja.chang0628@gmail.com>
2021-03-13 15:00:59 +0800

    flake 8 formatting

--- prepropy/imputation.py
+++ prepropy/imputation.py
@@ -23,6 +23,7 @@ class imputation:
     >>>test_df = pd.DataFrame([[np.nan,2,3],[2,np.nan,4],[5,6,7]])
     >>>imputer = imputation('mean')
     """
+
     def __init__(self, method):
         """
         Initialize the class
@@ -32,11 +33,11 @@ class imputation:
         method: str
             method we wish to do the imputing.
         """
-        if method not in ['mean', 'median', 'most_frequent']:
+        if method not in ["mean", "median", "most_frequent"]:
             raise KeyError("Method must be one of mean, median, most_frequent")
         self.method = method
         self.values = None
-    
+
     def fit(self, data):
         """
         Calculates the value to be imputated for each column in the dataframe and store them in self.values
@@ -58,20 +59,20 @@ class imputation:
         >>>imputer.fit(test_df)
         """
         if type(data) != pd.DataFrame:
-            raise TypeError('Input data must be a Pandas Dataframe')
+            raise TypeError("Input data must be a Pandas Dataframe")
         if data.empty:
-            raise ValueError('DataFrame cannot be empty')
-        if self.method == 'mean':
+            raise ValueError("DataFrame cannot be empty")
+        if self.method == "mean":
             if data.shape[1] != data.select_dtypes(include=np.number).shape[1]:
                 raise TypeError("All values in dataframe must be numeric")
             self.values = data.mean().values
-        elif self.method == 'median':
+        elif self.method == "median":
             if data.shape[1] != data.select_dtypes(include=np.number).shape[1]:
                 raise TypeError("All values in dataframe must be numeric")
             self.values = data.median().values
-        elif self.method == 'most_frequent':
+        elif self.method == "most_frequent":
             self.values = data.mode().values[0]
-            
+
     def fill(self, data_for_fill):
         """
         Fills the missing values in each column of the dataframe that we wish to fill.
@@ -80,7 +81,7 @@ class imputation:
         ----------
         data_for_fill: pandas.core.frame.DataFrame
             a pandas dataframe that we wish to fill the missing values with. Note that the number of columns(features) of the dataframe that we wish to fill must
-            equal to that of the one we fit the data with. In addition, order of the columns(features) for the dataframe we wish to fill should be the same as the one 
+            equal to that of the one we fit the data with. In addition, order of the columns(features) for the dataframe we wish to fill should be the same as the one
             we used to fit our class.
 
         Returns
@@ -107,9 +108,10 @@ class imputation:
         2  3.5   3.  4.666667
         """
         if len(self.values) != data_for_fill.shape[1]:
-            raise TypeError('DataFrame to be imputed must have the same number of columns as the fitted data')
+            raise TypeError(
+                "DataFrame to be imputed must have the same number of columns as the fitted data"
+            )
         data = data_for_fill.copy()
         for i in range(len(data.columns)):
-            data.iloc[:,i].fillna(self.values[i], inplace = True)
+            data.iloc[:, i].fillna(self.values[i], inplace=True)
         return data
-
--- tests/test_imputation.py
+++ tests/test_imputation.py
@@ -3,96 +3,107 @@ import pytest
 import pandas as pd
 import numpy as np
 
+
 def test_imputation():
-    """Group of tests that test for the output generated from impute
-    """
+    """Group of tests that test for the output generated from impute"""
     df1 = pd.DataFrame([[np.nan, 2, 3], [4, np.nan, 6], [10, 5, 9]])
     df2 = pd.DataFrame([[np.nan, 2, 3], [4, np.nan, 6], [10, 5, 9], [3, 15, 17]])
-    df3 = pd.DataFrame([[np.nan,'b','c'],['d',np.nan,'f'],['d','x',np.nan]])
-    df_m = pd.DataFrame([[np.nan,1,'c'],['d',np.nan,'f'],['d',3,np.nan]])
+    df3 = pd.DataFrame([[np.nan, "b", "c"], ["d", np.nan, "f"], ["d", "x", np.nan]])
+    df_m = pd.DataFrame([[np.nan, 1, "c"], ["d", np.nan, "f"], ["d", 3, np.nan]])
     df_s = pd.DataFrame([1])
     df1_t = pd.DataFrame([[7.0, 2, 3], [4, 3.5, 6], [10, 5, 9]])
-    df2_t = pd.DataFrame([[ 4.,  2.,  3.],[ 4.,  5.,  6.],[10.,  5.,  9.],[ 3., 15., 17.]])
-    df3_t = pd.DataFrame([['d', 'b', 'c'],['d', 'b', 'f'],['d', 'x', 'c']])
-    df_m_t = pd.DataFrame([['d', 1.0, 'c'],['d', 1.0, 'f'],['d', 3.0, 'c']]) 
+    df2_t = pd.DataFrame(
+        [[4.0, 2.0, 3.0], [4.0, 5.0, 6.0], [10.0, 5.0, 9.0], [3.0, 15.0, 17.0]]
+    )
+    df3_t = pd.DataFrame([["d", "b", "c"], ["d", "b", "f"], ["d", "x", "c"]])
+    df_m_t = pd.DataFrame([["d", 1.0, "c"], ["d", 1.0, "f"], ["d", 3.0, "c"]])
     # test for mean imputation
-    imputer = imputation('mean')
+    imputer = imputation("mean")
     imputer.fit(df1)
     df1_filled = imputer.fill(df1)
-    assert np.array_equal(df1_filled.to_numpy(), df1_t.to_numpy()), 'Generated Output are incorrect'
-    
+    assert np.array_equal(
+        df1_filled.to_numpy(), df1_t.to_numpy()
+    ), "Generated Output are incorrect"
+
     # test for median imputation
-    imputer = imputation('median')
+    imputer = imputation("median")
     imputer.fit(df2)
     df2_filled = imputer.fill(df2)
-    assert np.array_equal(df2_filled.to_numpy(), df2_t.to_numpy()), 'Generated Output are incorrect'
-    
+    assert np.array_equal(
+        df2_filled.to_numpy(), df2_t.to_numpy()
+    ), "Generated Output are incorrect"
+
     # test for most frequent imputation
-    imputer = imputation('most_frequent')
+    imputer = imputation("most_frequent")
     imputer.fit(df3)
     df3_filled = imputer.fill(df3)
-    assert np.array_equal(df3_filled.to_numpy(), df3_t.to_numpy()), 'Generated Output are incorrect'
-    
+    assert np.array_equal(
+        df3_filled.to_numpy(), df3_t.to_numpy()
+    ), "Generated Output are incorrect"
+
     # test for dataframe with non nan values
-    imputer = imputation('mean')
+    imputer = imputation("mean")
     imputer.fit(df1_t)
     df4_filled = imputer.fill(df1_t)
-    assert np.array_equal(df4_filled.to_numpy(), df1_t.to_numpy()), 'Generated Output are incorrect'
-    
+    assert np.array_equal(
+        df4_filled.to_numpy(), df1_t.to_numpy()
+    ), "Generated Output are incorrect"
+
     # test for mix type dataframe for most_frequent method
-    imputer = imputation('most_frequent')
+    imputer = imputation("most_frequent")
     imputer.fit(df_m)
     df5_filled = imputer.fill(df_m)
-    assert np.array_equal(df5_filled.to_numpy(), df_m_t.to_numpy()), 'Generated Output are incorrect'
-    
+    assert np.array_equal(
+        df5_filled.to_numpy(), df_m_t.to_numpy()
+    ), "Generated Output are incorrect"
+
     # test for dataframe where there is only a single value
-    imputer = imputation('mean')
+    imputer = imputation("mean")
     imputer.fit(df_s)
     df6_filled = imputer.fill(df_s)
-    assert np.array_equal(df6_filled.to_numpy(), df_s.to_numpy()), 'Generated Output are incorrect'
+    assert np.array_equal(
+        df6_filled.to_numpy(), df_s.to_numpy()
+    ), "Generated Output are incorrect"
 
 
 # Exception Handling
 def test_empty():
-    """Tests whether Class Method catches empty dataframe
-    """
+    """Tests whether Class Method catches empty dataframe"""
     emp = pd.DataFrame()
-    imputer = imputation('mean')
+    imputer = imputation("mean")
     with pytest.raises(ValueError):
         imputer.fit(emp)
 
+
 def test_method():
-    """Tests whether Class Method catches wrong method for imputation
-    """
+    """Tests whether Class Method catches wrong method for imputation"""
     with pytest.raises(KeyError):
-        imputer = imputation('hello')
+        imputer = imputation("hello")
+
+
 def test_pdtype():
-    """ Tests whether Class Method catches wrong datatype
-    """
+    """Tests whether Class Method catches wrong datatype"""
     with pytest.raises(TypeError):
-        imputer = imputation('mean')
+        imputer = imputation("mean")
         imputer.fit(1)
-    
+
+
 def test_pdtype2():
-    """Tests given method of imputations are mean and median, the data within DataFrame must be all numeric
-    """
-    test_df = pd.DataFrame([['a','b','c'],['d',np.nan,'f'],['z','x','y']])
+    """Tests given method of imputations are mean and median, the data within DataFrame must be all numeric"""
+    test_df = pd.DataFrame([["a", "b", "c"], ["d", np.nan, "f"], ["z", "x", "y"]])
     with pytest.raises(TypeError):
-        imputer = imputation('mean')
+        imputer = imputation("mean")
         imputer.fit(test_df)
     with pytest.raises(TypeError):
-        imputer = imputation('median')
+        imputer = imputation("median")
         imputer.fit(test_df)
+
 
 def test_length():
-    """Tests whether the dataframe to be imputed has the same length as the fitted dataframe
-    """
-    test_df1 = pd.DataFrame([['a','b','c'],['d',np.nan,'f'],['z','x','y']])
-    test_df2 = pd.DataFrame([['a','b'],['d',np.nan],['z','x']])
+    """Tests whether the dataframe to be imputed has the same length as the fitted dataframe"""
+    test_df1 = pd.DataFrame([["a", "b", "c"], ["d", np.nan, "f"], ["z", "x", "y"]])
+    test_df2 = pd.DataFrame([["a", "b"], ["d", np.nan], ["z", "x"]])
     with pytest.raises(TypeError):
-        imputer = imputation('most_frequent')
+        imputer = imputation("most_frequent")
         imputer.fit(test_df1)
         imputer.fill(test_df2)
-        
-    
-
